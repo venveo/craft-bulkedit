e17308aeeffd30050dd7f64520e8af15bf8f5689
Ransom <ransom@venveo.com>
2019-07-21 16:27:01 -0400

    Forgot a migration!

---
+++ src/migrations/m190721_201115_add_element_type.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace venveo\bulkedit\migrations;
+
+use Craft;
+use craft\db\Migration;
+
+/**
+ * m190721_201115_add_element_type migration.
+ */
+class m190721_201115_add_element_type extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $this->addColumn('{{%bulkedit_editcontext}}', 'elementType', $this->string()->notNull());
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('{{%bulkedit_editcontext}}', 'elementType');
+    }
+}
