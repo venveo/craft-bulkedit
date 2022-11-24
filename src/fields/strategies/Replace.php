4e38ac98db715be9399f93992015b88686df995c
nils&paul <info@nilsenpaul.nl>
2022-11-24 10:55:40 +0100

    Update Replace.php

--- src/fields/strategies/Replace.php
+++ src/fields/strategies/Replace.php
@@ -2,12 +2,13 @@
 
 namespace venveo\bulkedit\fields\strategies;
 
+use Craft;
 use venveo\bulkedit\base\FieldStrategyInterface;
 
 class Replace implements FieldStrategyInterface
 {
     public static function displayName(): string
     {
-        return 'Replace';
+        return Craft::t('bulk-edit', 'Replace');
     }
 }
